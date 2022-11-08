4da9d4f1114b799f8501423cab99c5ef04489759
Григорий <fash1on.pro@yandex.ru>
2022-11-08 09:53:49 +0300

    mk upds

---
+++ return negative.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class Kata
+{
+    public static int MakeNegative(int number)
+    {
+        if (number <= 0)
+        {
+            return number;
+        }
+        else
+        {
+            return -number;
+        }
+    }
+}
